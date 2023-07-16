2268eec54b790b6ef5216bc85d7aba07a5e26af4
smtdfc <135759660+smtdfc@users.noreply.github.com>
2023-07-17 01:23:17 +0700

    update server.js

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ fastify.register(require("@fastify/cors"))
 fastify.addHook('preHandler',authTokenVerify)
 router(fastify)
 
+
 fastify.listen({port:3000, host: "0.0.0.0" },
 	function(err, address) {
 		if (err) {
